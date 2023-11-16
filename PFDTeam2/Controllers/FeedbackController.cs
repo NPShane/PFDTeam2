a3c6f314fe1f08a3936b4d08eae3892bd54b5016
Shane Leong <s10239824@connect.np.edu.sg>
2023-11-16 19:03:33 +0800

    Added rudimentary, non-AI, relevance checks

--- PFDTeam2/Controllers/FeedbackController.cs
+++ PFDTeam2/Controllers/FeedbackController.cs
@@ -20,11 +20,17 @@ namespace PFDTeam2.Controllers
         public ActionResult ProcessFeedback(string feedback)
         {
             // logic for relevance
-            bool isRelevant = false;
+            bool isRelevant = ContainsAnyKeyword(feedback, new[] { "improvement", "onboarding", "goals" });
 
             staffDAL.AddFeedback(feedback, isRelevant, false);
 
-            return RedirectToAction("Index");
+            return RedirectToAction("Main", "Staff");
+        }
+
+        private bool ContainsAnyKeyword(string text, string[] keywords)
+        {
+            // Check if any of the keywords are present in the text (case-insensitive)
+            return keywords.Any(keyword => text.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0);
         }
     }
 }
